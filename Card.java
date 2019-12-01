05e6504c0a2387b7f77ae8cd23a72298be77bb81
Carlo <carlomadi311@gmail.com>
2019-12-02 00:55:25 +0800

    merge conflict

--- Card.java
+++ Card.java
@@ -7,4 +7,11 @@ class Card{
 	final static int MONSTER = 700;		//fixed na yung value ng damage nung cards
 	final static int MAGIC = 200;
 	final static int TRAP = 300;
+
+	Card(String type, int damage){		//constructor
+		this.type = type;
+		this.damage = damage;
+		this.heal = damage;
+		
+	}
 }
